24b1ea325555854a17c04e5ed9b9c379bdea4b84
ErikGaniev <eik.ganiev2000@gmail.com>
2022-12-21 03:13:15 +0500

    как найти оба значения

--- pv.py
+++ pv.py
@@ -35,8 +35,14 @@ def basa(s):
         ind = lis.index(mx)
         max_array.append([names[i], mx, ind, x[names[i + 1]][ind + 1], x[names[i]][ind + 1]])
         names_out.append(names[i])
-        w = sorted(enumerate(lis), key=lambda p: abs(p[1] - mx * tgd))
-        part_array.append((w[0][1], x[names[i]][w[0][0] + 1], w[1][1], x[names[i]][w[1][0] + 1]))
+        w1 = np.zeros_like(lis)
+        w2 = np.zeros_like(lis)
+        w1[0:ind] = lis[0:ind]
+        w2[ind:] = lis[ind:]
+        w1 = sorted(enumerate(w1), key=lambda p: abs(p[1] - mx * tgd))
+        w2 = sorted(enumerate(w2), key=lambda p: abs(p[1] - mx * tgd))
+        # print(w)
+        part_array.append([w1[0][1], x[names[i]][w1[0][0] + 1], w2[0][1], x[names[i]][w2[0][0] + 1]])
 
     return x, names_out, max_array, part_array
 
@@ -49,19 +55,34 @@ print(lsav)
 
 xoil, name_oil, array_oil, part_oil = basa("Нефти_нов")
 loil = len(array_oil)
+print(xoil)
 print(array_oil)
 print(part_oil)
-
-df = np.zeros((lsav, loil))
-# print(df)
-df = pd.DataFrame(df, index=name_sav, columns=name_oil)
-
-# print(df)
-
-# print(df[name_oil[0]][name_sav[0]])
+print(part_oil[0][0])
+graph(xoil, xoil.columns.tolist())
+df1 = np.zeros((lsav, loil))
+df1 = pd.DataFrame(df1, index=name_sav, columns=name_oil)
 
 for i in range(len(array_sav)):
     for j in range(len(array_oil)):
-        df[name_oil[j]][name_sav[i]] = np.fabs(array_oil[j][4] - array_sav[i][4])
+        df1[name_oil[j]][name_sav[i]] = np.fabs(array_oil[j][4] - array_sav[i][4])
 
-print(df)
+print(df1)
+
+df2 = np.zeros((lsav, loil))
+df2 = pd.DataFrame(df2, index=name_sav, columns=name_oil)
+
+for i in range(len(array_sav)):
+    for j in range(len(array_oil)):
+        if part_oil[j][1] > part_oil[j][3]:
+            swap = part_oil[j][3]
+            part_oil[j][3] = part_oil[j][1]
+            part_oil[j][1] = swap
+            swap = part_oil[j][2]
+            part_oil[j][2] = part_oil[j][0]
+            part_oil[j][0] = swap
+        if part_oil[j][1] <= array_sav[i][4] <= part_oil[j][3]:
+            df2[name_oil[j]][name_sav[i]] = 1
+        else:
+            df2[name_oil[j]][name_sav[i]] = 0
+print(df2.to_string())
