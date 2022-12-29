df0a9470d5abed2254325ace5d784b82ae211330
ErikGaniev <eik.ganiev2000@gmail.com>
2022-12-29 15:22:57 +0500

    if name

--- pv.py
+++ pv.py
@@ -59,14 +59,14 @@ def main():
     xsav, name_sav, array_sav, part_sav = basa(loc, label_pav)
     lsav = len(array_sav)
     # print(array_sav)
-    print(name_sav)
+    # print(name_sav)
     # print(lsav)
 
     xoil, name_oil, array_oil, part_oil = basa(loc, label_oil)
     loil = len(array_oil)
     # print(xoil["Арланское скв.456"].to_string())
     # print(array_oil)
-    print(part_oil)
+    # print(part_oil)
     # print(part_oil[0][0])
 
     # graph(xoil, xoil.columns.tolist())
@@ -77,7 +77,7 @@ def main():
         for j in range(len(array_oil)):
             df1[name_oil[j]][name_sav[i]] = np.fabs(array_oil[j][4] - array_sav[i][4])
 
-    print(df1)
+    # print(df1)
 
     df2 = np.zeros((lsav, loil))
     df2 = pd.DataFrame(df2, index=name_sav, columns=name_oil)
@@ -88,19 +88,19 @@ def main():
                 df2[name_oil[j]][name_sav[i]] = 1
             else:
                 df2[name_oil[j]][name_sav[i]] = 0
-    print(df2.to_string())
+    # print(df2.to_string())
 
     df3 = np.zeros((lsav, loil))
     df3 = pd.DataFrame(df3, index=name_sav, columns=name_oil)
 
-    print(part_sav[1][3], part_sav[1][1])
+    # print(part_sav[1][3], part_sav[1][1])
     for i in range(len(array_sav)):
         for j in range(len(array_oil)):
             if part_oil[j][3] <= part_sav[i][3] <= part_oil[j][1] or part_sav[i][3] <= part_oil[j][3] <= part_sav[i][1]:
                 df3[name_oil[j]][name_sav[i]] = 1
             else:
                 df3[name_oil[j]][name_sav[i]] = 0
-    print(df3)
+    # print(df3)
 
     with pd.ExcelWriter(label_pav + '.xlsx') as writer:
         df1.to_excel(writer, sheet_name='Sheet_name_1')
@@ -108,4 +108,5 @@ def main():
         df3.to_excel(writer, sheet_name='Sheet_name_3')
 
 
-main()
+if __name__ == '__main__':
+    main()
